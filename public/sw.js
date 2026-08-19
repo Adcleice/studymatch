879f4a98268bd7894ec6b4cd3e8010db19abd747
Adcleice <105397971+Adcleice@users.noreply.github.com>
2026-08-19 18:50:40 -0300

    refresh StudyMatch logo and icon cache

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 const CACHE='studymatch-v7';
-const SHELL=['/','/manifest.webmanifest?v=5','/studymatch-mark.svg?v=5','/studymatch-app-icon.svg?v=5'];
+const SHELL=['/','/manifest.webmanifest?v=7','/studymatch-mark.svg?v=7','/studymatch-logo.svg?v=7'];
 
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)));
