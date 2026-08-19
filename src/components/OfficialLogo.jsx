71dc7d54da5b5deab800c0311274e05489725388
Adcleice <105397971+Adcleice@users.noreply.github.com>
2026-08-19 17:25:21 -0300

    keep StudyMatch mark legible on light surfaces

--- src/components/OfficialLogo.jsx
+++ src/components/OfficialLogo.jsx
@@ -1,2 +1,2 @@
 import React from'react';
-export default function OfficialLogo({style={},className='',alt='StudyMatch'}){return <img src="/studymatch-mark.svg" alt={alt} className={className} style={{display:'block',objectFit:'contain',...style}}/>}
+export default function OfficialLogo({style={},className='',alt='StudyMatch'}){return <img src="/studymatch-mark.svg?v=5" alt={alt} className={className} style={{display:'block',objectFit:'contain',background:'#082A55',borderRadius:'28%',padding:'9%',boxSizing:'border-box',...style}}/>}
