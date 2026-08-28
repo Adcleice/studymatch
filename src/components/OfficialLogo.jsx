ab5388790d502c94bbadc9c74dabc2ea3a2db3fa
Adcleice <105397971+Adcleice@users.noreply.github.com>
2026-08-27 21:09:01 -0300

    Update official brand name

--- src/components/OfficialLogo.jsx
+++ src/components/OfficialLogo.jsx
@@ -1,2 +1,2 @@
 import React from'react';
-export default function OfficialLogo({style={},className='',alt='StudyMatch',withBackground=true}){const src=withBackground?'/studymatch-logo.svg?v=7':'/studymatch-mark.svg?v=7';return <img src={src} alt={alt} className={className} style={{display:'block',width:36,height:36,maxWidth:36,maxHeight:36,objectFit:'contain',flex:'0 0 36px',...style}}/>}
+export default function OfficialLogo({style={},className='',alt='Matchworking',withBackground=true}){const src='/studymatch-mark.svg?v=8';return <img src={src} alt={alt} className={className} style={{display:'block',width:36,height:36,maxWidth:36,maxHeight:36,objectFit:'contain',flex:'0 0 36px',...style}}/>}
