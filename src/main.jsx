c9cad8767015d762522a2b84063302e08cb215de
Adcleice <105397971+Adcleice@users.noreply.github.com>
2026-08-29 05:36:57 -0300

    Load focused forum launch styling

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import NetworkStatus from'./components/NetworkStatus.jsx';
 import{BrandProvider}from'./components/BrandProvider.jsx';
 import'./index.css';
 import'./a11y.css';
+import'./forum-inviting.css';
 
 if('serviceWorker'in navigator){window.addEventListener('load',()=>navigator.serviceWorker.register('/sw.js',{updateViaCache:'none'}).then(reg=>reg.update()).catch(()=>{}))}
 
