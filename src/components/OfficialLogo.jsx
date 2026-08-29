bb0e90f52e1db802dab8f78010e245bcc0730e75
Adcleice <105397971+Adcleice@users.noreply.github.com>
2026-08-28 21:28:30 -0300

    Use admin-selected logo across app

--- src/components/OfficialLogo.jsx
+++ src/components/OfficialLogo.jsx
@@ -1,2 +1,2 @@
-import React from'react';
-export default function OfficialLogo({style={},className='',alt='Matchworking',withBackground=true}){const src='/studymatch-mark.svg?v=8';return <img src={src} alt={alt} className={className} style={{display:'block',width:36,height:36,maxWidth:36,maxHeight:36,objectFit:'contain',flex:'0 0 36px',...style}}/>}
+import React from'react';import{useBrand}from'./BrandProvider.jsx';
+export default function OfficialLogo({style={},className='',alt='Matchworking'}){const b=useBrand(),src=b.logo_url||'/studymatch-mark.svg?v=8';return <img src={src} alt={alt} className={className} style={{display:'block',width:36,height:36,maxWidth:36,maxHeight:36,objectFit:'contain',flex:'0 0 36px',...style}}/>}
