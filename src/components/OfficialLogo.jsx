ce6a4639e181c8e083b556b2a91b8315edb4c820
Adcleice <105397971+Adcleice@users.noreply.github.com>
2026-08-28 21:43:13 -0300

    Support contextual logo overrides

--- src/components/OfficialLogo.jsx
+++ src/components/OfficialLogo.jsx
@@ -1,2 +1,2 @@
 import React from'react';import{useBrand}from'./BrandProvider.jsx';
-export default function OfficialLogo({style={},className='',alt='Matchworking'}){const b=useBrand(),src=b.logo_url||'/studymatch-mark.svg?v=8';return <img src={src} alt={alt} className={className} style={{display:'block',width:36,height:36,maxWidth:36,maxHeight:36,objectFit:'contain',flex:'0 0 36px',...style}}/>}
+export default function OfficialLogo({style={},className='',alt='Matchworking',context='default'}){const b=useBrand(),override=b.logo_overrides?.[context],src=override||b.logo_url||'/studymatch-mark.svg?v=8';return <img src={src} alt={alt} className={className} style={{display:'block',width:36,height:36,maxWidth:36,maxHeight:36,objectFit:'contain',flex:'0 0 36px',...style}}/>}
